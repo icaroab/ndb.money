6a775edca3eac2819c927c93d99044cf9355ce76
icaroab <sanjose90318@gmail.com>
2024-03-28 00:06:19 -0500

    auction.js

---
+++ src/apollo/graghqls/querys/Auction.js
@@ -0,0 +1,69 @@
+import { gql } from "@apollo/client"
+
+export const GET_AUCTION = gql`
+    query {
+        getAuctions {
+            round
+            startedAt
+            endedAt
+            totalToken
+            minPrice
+            token
+            sold
+            status
+        }
+    }
+`
+
+export const GET_AUCTION_BY_NUMBER = gql`
+    query getAuctionByNumber($round: Int!) {
+        getAuctionByNumber(round: $round) {
+            id
+            regDate
+            updateDate
+            deleted
+            round
+            startedAt
+            endedAt
+            totalToken
+            minPrice
+            token
+            sold
+            stats {
+                qty
+                win
+                fail
+            }
+            status
+        }
+    }
+`
+export const GET_AUCTION_BY_STATUS = gql`
+    query getAuctionByStatus($status: Int!) {
+        getAuctionByStatus(status: $status) {
+            number
+            token
+            status
+        }
+    }
+`
+export const GET_AUCTIONS = gql`
+    query {
+        getAuctions {
+            id
+            round
+            startedAt
+            endedAt
+            totalToken
+            minPrice
+            token
+            sold
+            stats {
+                qty
+                win
+                fail
+            }
+            status
+        }
+    }
+`
