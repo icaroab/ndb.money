6c69a91d485418407e89487a4f10a419ee2a8012
icaroab <sanjose90318@gmail.com>
2024-04-10 05:27:58 -0500

    avatar.js

---
+++ src/apollo/graghqls/querys/AvatarComponent.js
@@ -0,0 +1,42 @@
+import { gql } from '@apollo/client';
+
+export const GET_AVATAR_COMPONENTS = gql`
+    query {
+        getAvatarComponents {
+            groupId
+            compId
+            tierLevel
+            price
+            limited
+            purchased
+            svg
+            width
+            top
+            left
+        }
+    }
+`;
+
+export const GET_AVATARS = gql`
+    query {
+        getAvatars {
+            id
+            fname
+            surname
+            avatarSet {
+                groupId
+                compId
+            }
+            hairColor
+            skillSet {
+                name
+                rate
+            }
+            factsSet {
+                topic
+                detail
+            }
+            details
+        }
+    }
+`;
