657a4788df5cb8a4104562bdcfec5c98ead8978f
deep-explorer <explorer0933@gmail.com>
2024-04-10 08:24:58 -0500

    testse

---
+++ src/temp/temp.js
@@ -0,0 +1,19 @@
+className="btn-mobile">
+                            <div className="">
+                                <button
+                                    className="btn-green"
+                                    onClick={() => {
+                                        navigate("/pre-sale/auction")
+                                    }}
+                                >
+                                    Buy
+                                </button>
+                            </div>
+                            
+                            <div className="py-3 text-decoration-underline text-center text-light">
+                                Learn more
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
